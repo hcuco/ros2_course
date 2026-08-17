c8a5ea4124f1f5f6017bd9e9b3d1862e870dddb6
Henrique Cuco <henriquecuco04@gmail.com>
2026-08-17 19:00:42 -0300

    First ROS 2 program and introduction to ROS 2 tools

--- src/build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
+++ src/build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
@@ -7,7 +7,7 @@ class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
         self.counter_ = 0
-        self.get_logger().info("Hello world aaaaaa")
+        self.get_logger().info("Hello world")
         self.create_timer(1.0, self.timer_callback)
 
     def timer_callback(self):
