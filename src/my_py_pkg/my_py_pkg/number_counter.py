140fad18f00607248f3b546076b710d680ce3eea
Henrique Cuco <henriquecuco04@gmail.com>
2026-08-19 13:27:59 -0300

    Activity 3 - ROS 2 Services

--- src/my_py_pkg/my_py_pkg/number_counter.py
+++ src/my_py_pkg/my_py_pkg/number_counter.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from example_interfaces.srv import SetBool
  
 class NumberCounterNode(Node): 
     def __init__(self):
@@ -11,6 +12,8 @@ class NumberCounterNode(Node):
             Int64, "number", self.callback_number,10)
         self.publisher_ = self.create_publisher(Int64,"number_count",10)
         self.get_logger().info("Number Counter has been started.")
+        self.server_ = self.create_service(SetBool, "reset_counter", self.callback_reset_counter)
+        self.get_logger().info("Reset Counter server has been started.")
 
     def callback_number(self, msg: Int64):
         self.counter += msg.data
@@ -18,8 +21,14 @@ class NumberCounterNode(Node):
         new_msg.data = self.counter
 
         self.publisher_.publish(new_msg)
- 
- 
+
+    def callback_reset_counter(self, request: SetBool.Request, response: SetBool.Response):
+        if request.data == True:
+            self.counter = 0
+            response.success = True
+            self.get_logger().info("Number Counter has been reset.")
+        return response
+
 def main(args=None):
     rclpy.init(args=args)
     node = NumberCounterNode() 
